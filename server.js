a37bfbaf937950c52aeb98a2ed816462008c81fe
navjeet4real <navjeetkajal.2594@gmail.com>
2023-02-27 15:15:19 +0530

    socket connection

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const User  = require("./models/user")
+const User = require("./models/user");
 const { Server } = require("socket.io");
+const FriendRequest = require("./models/friendRequest");
 
 dotenv.config({ path: "./config.env" });
 
 process.on("uncaughtException", (err) => {
   console.log(err);
-  process.exit(1);
+  console.log("UNCAUGHT Exception! Shutting down ...");
+  process.exit(1); // Exit Code 1 indicates that a container shut down, either because of an application failure.
 });
 
 const http = require("http");
@@ -20,28 +22,6 @@ const io = new Server(server, {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
-});
-
-io.on("connection", async (socket) => {
-    // console.log(socket,'socket')
-    const user_id = socket.handshake.query['user_id ']
-
-    const socket_id = socket.id;
-    console.log(`User conneccted ${socket_id} !!!`)
-
-    if(user_id){
-        await User.findByIdAndUpdate(user_id,{socket_id})
-    }
-    socket.on("friend_request", async(data) => {
-        console.log(data.to);
-
-        const to = await User.findById(data.to)
-
-        io.to(to.socket_id).emit("new_friend_request",{
-            
-        })
-         
-    })
 });
 
 const DB = process.env.DBURI.replace("<PASSWORD>", process.env.DBPASSWORD);
@@ -60,9 +40,80 @@ server.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
 
+// Add this
+// Listen for when the client connects via socket.io-client
+io.on("connection", async (socket) => {
+  console.log(JSON.stringify(socket.handshake.query));
+  const user_id = socket.handshake.query["user_id"];
+
+  console.log(`User connected ${socket.id}`);
+
+  console.log(Boolean(user_id), user_id);
+
+  if (Boolean(user_id)) {
+    await User.findByIdAndUpdate(user_id, { socket_id: socket.id });
+  }
+
+  // We can write our socket event listeners in here...
+  socket.on("friend_request", async (data) => {
+    console.log(data.to);
+
+    const to = await User.findById(data.to).select("socket_id");
+    const from = await User.findById(data.from).select("socket_id");
+
+    // create a friend request
+    await FriendRequest.create({
+      sender: data.from,
+      recipient: data.to,
+    });
+    // emit event request received to recipient
+    io.to(to.socket_id).emit("new_friend_request", {
+      message: "New friend request received",
+    });
+    io.to(from.socket_id).emit("request_sent", {
+      message: "Request Sent successfully!",
+    });
+  });
+
+  socket.on("accept_request", async (data) => {
+    // accept friend request => add ref of each other in friends array
+    console.log(data);
+    const request_doc = await FriendRequest.findById(data.request_id);
+
+    console.log(request_doc);
+
+    const sender = await User.findById(request_doc.sender);
+    const receiver = await User.findById(request_doc.recipient);
+
+    sender.friends.push(request_doc.recipient);
+    receiver.friends.push(request_doc.sender);
+
+    await receiver.save({ new: true, validateModifiedOnly: true });
+    await sender.save({ new: true, validateModifiedOnly: true });
+
+    await FriendRequest.findByIdAndDelete(data.request_id);
+
+    // delete this request doc
+    // emit event to both of them
+
+    // emit event request accepted to both
+    io.to(sender.socket_id).emit("request_accepted", {
+      message: "Friend Request Accepted",
+    });
+    io.to(receiver.socket_id).emit("request_accepted", {
+      message: "Friend Request Accepted",
+    });
+  });
+  socket.on("end", function () {
+    console.log("closing connection");
+    socket.disconnect(0);
+  });
+});
+
 process.on("unhandledRejection", (err) => {
   console.log(err);
+  console.log("UNHANDLED REJECTION! Shutting down ...");
   server.close(() => {
-    process.exit(1);
+    process.exit(1); //  Exit Code 1 indicates that a container shut down, either because of an application failure.
   });
 });
